634c4126b06caaf8d57098a3f35e38c27084fe88
Linesh Jose <lineshjose@gmail.com>
2013-02-20 17:19:05 +0530

    Update php-browser-detection.php

--- php-browser-detection.php
+++ php-browser-detection.php
@@ -1,24 +1,26 @@
 <?php
 /*
-  Name: Simple PHP Browser Detection script.
+	Name: Simple PHP Browser Detection script.
 	Version : 13.02
 	Author: Linesh Jose
 	Url: http://lineshjose.com
 	Email: lineshjose@gmail.com
 	Donate:  http://bit.ly/donate-linesh
-  github: https://github.com/lineshjose
+	github: https://github.com/lineshjose
 	Copyright: Copyright (c) 2013 LineshJose.com
-
+	
 	Note: This script is free; you can redistribute it and/or modify  it under the terms of the GNU General Public License as published by 
 		the Free Software Foundation; either version 2 of the License, or (at your option) any later version.This script is distributed in the hope 
 		that it will be useful,    but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 		See the  GNU General Public License for more details.
 
+-----------------------------------------------------------
 
 	This function to get the current browser info
 	@param $arg : returns reuestd browser property. Eg: platform, name, version,
 	@param $agent: it is the $_SERVER['HTTP_USER_AGENT'] value
-*/	
+*/
+	
 function get_browser_info($arg='',$agent='')
 {
 	if(empty($agent) ) {
